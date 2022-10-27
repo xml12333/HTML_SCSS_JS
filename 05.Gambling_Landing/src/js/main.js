9da57ed964e99cd33c784f17a1f0b2e48d633187
Nik T <xml12333@gmail.com>
2022-10-27 10:27:37 +0300

    05 add pricing

--- 05.Gambling_Landing/src/js/main.js
+++ 05.Gambling_Landing/src/js/main.js
@@ -1,1 +1,17 @@
 // Custom scripts
+//Switcher
+function togglePrice(){
+const billing = document.querySelector('.billing')
+const billingItems = document.querySelectorAll('.billing__item')
+billing.addEventListener('click',()=>{
+    billing.classList.toggle('active')
+    if (!billing.classList.contains('active')){
+        billingItems[0].classList.add('active')
+        billingItems[2].classList.remove('active')
+    }else{
+        billingItems[2].classList.add('active')
+        billingItems[0].classList.remove('active')
+    }
+})
+}
+togglePrice()
