9d88bf11c9c4338bd8236398f81de199fb5c2f42
Nik T <xml12333@gmail.com>
2022-11-24 09:21:29 +0200

    07 continue

--- 07.Portfolio/src/js/main.js
+++ 07.Portfolio/src/js/main.js
@@ -1,135 +1,162 @@
 // Custom scripts
 // Мобильное меню бургер
 function burgerMenu() {
-  const burger = document.querySelector('.burger')
-  const menu = document.querySelector('.menu')
-  const body = document.querySelector('body')
-  burger.addEventListener('click', () => {
-    if (!menu.classList.contains('active')) {
-      menu.classList.add('active')
-      burger.classList.add('active-burger')
-      body.classList.add('locked')
+  const burger = document.querySelector(".burger");
+  const menu = document.querySelector(".menu");
+  const body = document.querySelector("body");
+  burger.addEventListener("click", () => {
+    if (!menu.classList.contains("active")) {
+      menu.classList.add("active");
+      burger.classList.add("active-burger");
+      body.classList.add("locked");
     } else {
-      menu.classList.remove('active')
-      burger.classList.remove('active-burger')
-      body.classList.remove('locked')
+      menu.classList.remove("active");
+      burger.classList.remove("active-burger");
+      body.classList.remove("locked");
     }
-  })
+  });
   // Вот тут мы ставим брейкпоинт навбара
-  window.addEventListener('resize', () => {
+  window.addEventListener("resize", () => {
     if (window.innerWidth > 991.98) {
-      menu.classList.remove('active')
-      burger.classList.remove('active-burger')
-      body.classList.remove('locked')
+      menu.classList.remove("active");
+      burger.classList.remove("active-burger");
+      body.classList.remove("locked");
     }
-  })
+  });
 }
-burgerMenu()
-
+burgerMenu();
 
 // Вызываем эту функцию, если нам нужно зафиксировать меню при скролле.
 function fixedNav() {
-  const nav = document.querySelector('nav')
+  const nav = document.querySelector("nav");
 
   // тут указываем в пикселях, сколько нужно проскроллить что бы наше меню стало фиксированным
-  const breakpoint = 1
+  const breakpoint = 1;
   if (window.scrollY >= breakpoint) {
-    nav.classList.add('fixed__nav')
+    nav.classList.add("fixed__nav");
   } else {
-    nav.classList.remove('fixed__nav')
+    nav.classList.remove("fixed__nav");
   }
 }
-window.addEventListener('scroll', fixedNav)
-
+window.addEventListener("scroll", fixedNav);
 
 //tabs
-function tabs(headerSelector, tabSelector, contentSelector, activeClass, display='flex') {
+function tabs(
+  headerSelector,
+  tabSelector,
+  contentSelector,
+  activeClass,
+  display = "flex"
+) {
   const header = document.querySelector(headerSelector),
-        tab = document.querySelectorAll(tabSelector),
-        content = document.querySelectorAll(contentSelector)
+    tab = document.querySelectorAll(tabSelector),
+    content = document.querySelectorAll(contentSelector);
   function hideTabContent() {
-      content.forEach(item => {
-          item.style.display = 'none'
-      });
-      tab.forEach(item => {
-          item.classList.remove(activeClass)
-      });
+    content.forEach((item) => {
+      item.style.display = "none";
+    });
+    tab.forEach((item) => {
+      item.classList.remove(activeClass);
+    });
   }
   function showTabContent(i = 0) {
-     content[i].style.display = display
-     tab[i].classList.add(activeClass)
+    content[i].style.display = display;
+    tab[i].classList.add(activeClass);
   }
-  hideTabContent()
-  showTabContent()
-  header.addEventListener('click', e => {
-      const target = e.target
-      if (target.classList.contains(tabSelector.replace(/\./, '')) || 
-      target.parentNode.classList.contains(tabSelector.replace(/\./, ''))) {
-          tab.forEach((item, i) => {
-              if ( target == item || target.parentNode == item ) {
-                  hideTabContent()
-                  showTabContent(i)
-              }
-          });
-      }
-  })
+  hideTabContent();
+  showTabContent();
+  header.addEventListener("click", (e) => {
+    const target = e.target;
+    if (
+      target.classList.contains(tabSelector.replace(/\./, "")) ||
+      target.parentNode.classList.contains(tabSelector.replace(/\./, ""))
+    ) {
+      tab.forEach((item, i) => {
+        if (target == item || target.parentNode == item) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  });
 }
 
 // ПЕРВЫЙ аргумент - класс всего нашего хедера табов.
 // ВТОРОЙ аргумент - класс конкретного элемента, при клике на который будет переключатся таб.
 // ТРЕТИЙ аргумент - класс того блока, который будет переключаться.
 // ЧЕТВЕРТЫЙ аргумент - класс активности, который будет добавлятся для таба, который сейчас активен.
-tabs( '.tabs__header' ,'.tabs__header-item', '.tabs__content-item', 'active')
-
+tabs(".tabs__header", ".tabs__header-item", ".tabs__content-item", "active");
 
 // Аккордеон
 function accordion() {
-  const items = document.querySelectorAll('.accordion__item-trigger')
-  items.forEach(item => {
-      item.addEventListener('click', () => {
-          const parent = item.parentNode
-          if (parent.classList.contains('accordion__item-active')) {
-              parent.classList.remove('accordion__item-active')
-          } else {
-              document
-                  .querySelectorAll('.accordion__item')
-                  .forEach(child => child.classList.remove('accordion__item-active'))   
-              parent.classList.add('accordion__item-active')
-          }
-      })
-  })
+  const items = document.querySelectorAll(".accordion__item-trigger");
+  items.forEach((item) => {
+    item.addEventListener("click", () => {
+      const parent = item.parentNode;
+      if (parent.classList.contains("accordion__item-active")) {
+        parent.classList.remove("accordion__item-active");
+      } else {
+        document
+          .querySelectorAll(".accordion__item")
+          .forEach((child) => child.classList.remove("accordion__item-active"));
+        parent.classList.add("accordion__item-active");
+      }
+    });
+  });
 }
-accordion() 
-
+accordion();
 
 //modal
 
 // Модальное окно
 function bindModal(trigger, modal, close) {
-  trigger = document.querySelector(trigger),
-    modal = document.querySelector(modal),
-    close = document.querySelector(close)
+  (trigger = document.querySelector(trigger)),
+    (modal = document.querySelector(modal)),
+    (close = document.querySelector(close));
 
-  const body = document.body
+  const body = document.body;
 
-  trigger.addEventListener('click', e => {
-    e.preventDefault()
-    modal.style.display = 'flex'
-    body.classList.add('locked')
+  trigger.addEventListener("click", (e) => {
+    e.preventDefault();
+    modal.style.display = "flex";
+    body.classList.add("locked");
   });
-  close.addEventListener('click', () => {
-    modal.style.display = 'none'
-     body.classList.remove('locked')
+  close.addEventListener("click", () => {
+    modal.style.display = "none";
+    body.classList.remove("locked");
   });
-  modal.addEventListener('click', e => {
+  modal.addEventListener("click", (e) => {
     if (e.target === modal) {
-      modal.style.display = 'none'
-       body.classList.remove('locked')
+      modal.style.display = "none";
+      body.classList.remove("locked");
     }
-  })
+  });
 }
 
 // ПЕРВЫЙ аргумент - класс кнопки, при клике на которую будет открываться модальное окно.
 // ВТОРОЙ аргумент - класс самого модального окна.
 // ТРЕТИЙ аргумент - класс кнопки, при клике на которую будет закрываться модальное окно.
-bindModal('.modal__btn', '.modal__wrapper', '.modal__close')
+bindModal(".modal__btn", ".modal__wrapper", ".modal__close");
+
+//Swiper
+const swiper = new Swiper(".swiper", {
+  spaceBetween: 20,
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".arrow__btn-next",
+    prevEl: ".arrow__btn-prev",
+  },
+
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+    },
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 2,
+    },
+  },
+});
--- 07.Portfolio/src/js/scripts.js
+++ 07.Portfolio/src/js/scripts.js
@@ -1,2 +1,3 @@
 // Custom Scripts
+@@include('./libs/swiper(lib).js')
 @@include('main.js')
